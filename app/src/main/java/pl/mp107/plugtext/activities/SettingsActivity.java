bc043664e029e8c6a7c1794ca09703096ebc6cae
mp107 <mp107@o2.pl>
2018-02-07 19:23:32 +0100

    Added settings menu (code part) Now settings menu is visible

--- app/src/main/java/pl/mp107/plugtext/activities/SettingsActivity.java
+++ app/src/main/java/pl/mp107/plugtext/activities/SettingsActivity.java
@@ -1,7 +1,11 @@
 package pl.mp107.plugtext.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import pl.mp107.plugtext.R;
 
@@ -11,5 +15,35 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        Intent intent;
+
+        switch (id) {
+            case R.id.action_cancel_settings:
+                Toast.makeText(SettingsActivity.this,R.string.action_cancel,Toast.LENGTH_SHORT).show();
+                // TODO
+                return true;
+            case R.id.action_save_settings:
+                Toast.makeText(SettingsActivity.this,R.string.action_save,Toast.LENGTH_SHORT).show();
+                // TODO
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
