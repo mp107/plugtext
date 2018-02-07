c1973d30e10729f7050595a5659c5e571eafdaf9
mp107 <mp107@o2.pl>
2018-02-07 17:51:50 +0100

    Added empty activities

---
+++ app/src/main/java/pl/mp107/plugtext/activities/SettingsActivity.java
@@ -0,0 +1,15 @@
+package pl.mp107.plugtext.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pl.mp107.plugtext.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
